beeb65cd217b68e67fa421795cbcbb9024b121cc
Gabriel Fitzgarrald <gfreefitz@gmail.com>
2024-06-21 14:58:16 -0600

    testing new api /

--- src/index.js
+++ src/index.js
@@ -11,13 +11,20 @@ app.use(express.json());
 
 // /hello endpoint
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+    res.json({ message: 'Hello, World! Welcome to the Danger zone!!!' });
 });
 
 // /time endpoint
 app.get('/time', (req, res) => {
     const currentTime = new Date().toISOString();
     res.json({ currentTime });
+});
+
+
+// /games
+app.get('/games', (req,res) => {
+    const gameWow = "World of Warcraft - Wowhead";
+    res.json({ message: 'https://www.wowhead.com/classic/guide/season-of-discovery/classes/priest/healer-bis-gear-pve'});
 });
 
 app.listen(port, () => {
